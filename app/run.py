5ceb0d843ee6a25bf636ecd43451d436bd7088c4
rahul ray <ray.rahul.8101@gmail.com>
2022-04-20 13:35:45 +0530

    first commit2

--- app/run.py
+++ app/run.py
@@ -154,7 +154,7 @@ def go():
 
 
 def main():
-    # app.run(host='0.0.0.0', port=3001, debug=True)
+    # app.run(host='0.0.0.0', port=3001, debug=True)k
     app.run()
 
 if __name__ == '__main__':
